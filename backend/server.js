b965e032cde1ee8bda1aa968171bc049127bbe73
dhroov-coder <vermadhroov1@gmail.com>
2026-01-08 13:01:22 +0530

    add ping route to prevent render sleep

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,14 @@ app.get("/", (req, res) => {
   res.send("Backend running...");
 });
 
+/* ======================
+   PING (KEEP ALIVE)
+====================== */
+app.get("/ping", (req, res) => {
+  res.status(200).send("Server awake 🚀");
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
